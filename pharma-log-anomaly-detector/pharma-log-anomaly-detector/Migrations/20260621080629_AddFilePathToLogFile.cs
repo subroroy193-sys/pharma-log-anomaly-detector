f47d395199f1b85437a7fcbe988c57942f0914a7
roy <subroroy193@gmail.com>
2026-06-21 13:42:28 +0530

    Update migrations to include FilePath attribute in LogFile

---
+++ pharma-log-anomaly-detector/pharma-log-anomaly-detector/Migrations/20260621080629_AddFilePathToLogFile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pharma_log_anomaly_detector.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFilePathToLogFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "LogFiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "LogFiles");
+        }
+    }
+}
